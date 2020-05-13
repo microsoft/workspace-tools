fde649baeb54344d92a3ff9959c64f6f22ba5929
Ken Chau <kchau@microsoft.com>
2020-05-12 17:54:23 -0700

    use the matcher api correctly

--- src/scope.ts
+++ src/scope.ts
@@ -2,5 +2,5 @@ import { PackageInfos } from "./types/PackageInfo";
 import matcher from "matcher";
 
 export function getScopedPackages(scopes: string[], packages: PackageInfos) {
-  return matcher.isMatch(Object.keys(packages), scopes);
+  return matcher(Object.keys(packages), scopes);
 }
