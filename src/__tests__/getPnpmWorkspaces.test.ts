fe1302e9ce90891bf8355e7f87ffd419d37e60a9
Benjamin Weggersen <bewegger@microsoft.com>
2020-05-15 16:51:46 +0200

    Add tests

---
+++ src/__tests__/getPnpmWorkspaces.test.ts
@@ -0,0 +1,19 @@
+import path from "path";
+
+import { setupFixture } from "../helpers/setupFixture";
+import { getPnpmWorkspaces } from "../getWorkspaces/pnpmWorkspaces";
+
+describe("getPnpmWorkspaces()", () => {
+  it("gets the name and path of the workspaces", async () => {
+    const packageRoot = await setupFixture("monorepo-pnpm");
+    const workspacesPackageInfo = getPnpmWorkspaces(packageRoot);
+
+    const packageAPath = path.join(packageRoot, "packages", "package-a");
+    const packageBPath = path.join(packageRoot, "packages", "package-b");
+
+    expect(workspacesPackageInfo).toEqual([
+      { name: "package-a", path: packageAPath },
+      { name: "package-b", path: packageBPath },
+    ]);
+  });
+});
---
+++ src/__tests__/getYarnWorkspaces.test.ts
@@ -0,0 +1,19 @@
+import path from "path";
+
+import { setupFixture } from "../helpers/setupFixture";
+import { getYarnWorkspaces } from "../getWorkspaces/yarnWorkspaces";
+
+describe("getYarnWorkspaces()", () => {
+  it("gets the name and path of the workspaces", async () => {
+    const packageRoot = await setupFixture("monorepo");
+    const workspacesPackageInfo = getYarnWorkspaces(packageRoot);
+
+    const packageAPath = path.join(packageRoot, "packages", "package-a");
+    const packageBPath = path.join(packageRoot, "packages", "package-b");
+
+    expect(workspacesPackageInfo).toEqual([
+      { name: "package-a", path: packageAPath },
+      { name: "package-b", path: packageBPath },
+    ]);
+  });
+});
