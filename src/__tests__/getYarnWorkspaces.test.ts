a310abc8eaddb42b849d71ff43d6cadc75b5bfd4
Kenneth Chau <34725+kenotron@users.noreply.github.com>
2020-05-26 09:54:55 -0700

    Globby fixes - be able to match packages spec of an individual folder (#4)
    * fixes an issue with globby so that workspaces matches are found correctly
    
    * Change files
    
    * erase commented code

--- src/__tests__/getYarnWorkspaces.test.ts
+++ src/__tests__/getYarnWorkspaces.test.ts
@@ -16,4 +16,19 @@ describe("getYarnWorkspaces()", () => {
       { name: "package-b", path: packageBPath },
     ]);
   });
+
+  it("gets the name and path of the workspaces against a packages spec of an individual package", async () => {
+    const packageRoot = await setupFixture("monorepo-globby");
+    const workspacesPackageInfo = getYarnWorkspaces(packageRoot);
+
+    const packageAPath = path.join(packageRoot, "packages", "package-a");
+    const packageBPath = path.join(packageRoot, "packages", "package-b");
+    const individualPath = path.join(packageRoot, "packages", "individual");
+
+    expect(workspacesPackageInfo).toMatchObject([
+      { name: "individual", path: individualPath },
+      { name: "package-a", path: packageAPath },
+      { name: "package-b", path: packageBPath },
+    ]);
+  });
 });
--- src/getPackagePaths.ts
+++ src/getPackagePaths.ts
@@ -5,13 +5,17 @@ export function getPackagePaths(
   workspacesRoot: string,
   packages: string[]
 ): string[] {
-  const packagePaths = packages.map(glob =>
-    globby.sync(glob, {
-      cwd: workspacesRoot,
-      onlyDirectories: true,
-      absolute: true
-    })
-  );
+  const packagePaths = packages.map((glob) => {
+    const globbed = globby
+      .sync(path.join(glob, "package.json").replace(/\\/g, "/"), {
+        cwd: workspacesRoot,
+        absolute: true,
+        ignore: ["**/node_modules/**"],
+      })
+      .map((p) => path.dirname(p));
+
+    return globbed;
+  });
 
   /*
    * fast-glob returns unix style path,
@@ -21,5 +25,5 @@ export function getPackagePaths(
     .reduce((acc, cur) => {
       return [...acc, ...cur];
     })
-    .map(p => path.join(p));
+    .map((p) => path.join(p));
 }
