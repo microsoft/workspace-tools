3fa60635ecd728b9f65a0e1a8eb1a84e565b5ab4
Ken Chau <kchau@microsoft.com>
2020-09-15 10:09:21 -0700

    updated algorithm to match scoped or unscoped searches

--- src/scope.ts
+++ src/scope.ts
@@ -3,11 +3,11 @@ import matcher from "matcher";
 /**
  * Searches all package names based on "scoping" (i.e. "scope" in the sense of inclusion)
  * NOTE: scoping is different than package scopes (@scope/package)
- * @param searchScopes
+ * @param search
  * @param packages
  */
 export function getScopedPackages(
-  searchScopes: string[],
+  search: string[],
   packages: { [pkg: string]: unknown } | string[]
 ) {
   const packageNames = Array.isArray(packages)
@@ -16,26 +16,34 @@ export function getScopedPackages(
 
   const barePackageMap: { [key: string]: string[] } = {};
 
-  let inputs = packageNames;
-
-  // Step 1: create a map of bare package name -> list of full package names
+  // create a map of bare package name -> list of full package names
   // NOTE: do not perform barePackageMap lookup if any of the "scopes" arg starts with "@"
-  if (!searchScopes.find((scope) => scope.startsWith("@"))) {
-    for (const pkg of packageNames) {
-      const bare = pkg.replace(/^@[^/]+\//, "");
-      barePackageMap[bare] = barePackageMap[bare] || [];
-      barePackageMap[bare].push(pkg);
-    }
-    inputs = Object.keys(barePackageMap);
+  for (const pkg of packageNames) {
+    const bare = pkg.replace(/^@[^/]+\//, "");
+    barePackageMap[bare] = barePackageMap[bare] || [];
+    barePackageMap[bare].push(pkg);
   }
 
-  // Step 2: do the matcher algorithm (with support for wildcards like *)
-  const matched = matcher(inputs, searchScopes);
+  const results = new Set<string>();
 
-  // Step 3: reconstruct the list of full package names
-  let results: string[] = [];
-  for (const bare of matched) {
-    results = results.concat(barePackageMap[bare]);
+  // perform a package-scoped search (e.g. search is @scope/foo*)
+  const scopedSearch = search.filter((needle) => needle.startsWith("@"));
+  if (scopedSearch.length > 0) {
+    const matched = matcher(packageNames, scopedSearch);
+    for (const pkg of matched) {
+      results.add(pkg);
+    }
+  }
+
+  // perform a package-unscoped search (e.g. search is foo*)
+  const unscopedSearch = search.filter((needle) => !needle.startsWith("@"));
+  if (unscopedSearch.length > 0) {
+    let matched = matcher(Object.keys(barePackageMap), unscopedSearch);
+    for (const bare of matched) {
+      for (const pkg of barePackageMap[bare]) {
+        results.add(pkg);
+      }
+    }
   }
 
   return results;
