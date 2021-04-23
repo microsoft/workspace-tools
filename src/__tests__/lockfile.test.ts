63f590a1227dec6a45a6e8d3624e766ec08c3b89
Kenneth Chau <34725+kenotron@users.noreply.github.com>
2021-04-23 16:02:59 -0700

    Lazy load the lockfile implementations (#41)
    * move lockfile implementations behind a lazy load in lockfile.ts
    
    * fixtures to be private
    
    * Change files

--- src/__tests__/lockfile.test.ts
+++ src/__tests__/lockfile.test.ts
@@ -1,3 +1,4 @@
+import * as path from "path";
 import { setupFixture } from "../helpers/setupFixture";
 import { parseLockFile } from "../lockfile";
 
@@ -14,6 +15,24 @@ describe("parseLockFile()", () => {
 
     await expect(parseLockFile(packageRoot)).rejects.toThrow(
       "You do not have either yarn.lock nor pnpm-lock.yaml. Please use one of these package managers"
+    );
+  });
+
+  it("parses combined ranges in yarn.lock", async () => {
+    const packageRoot = await setupFixture("basic-yarn");
+    const parsedLockeFile = await parseLockFile(packageRoot);
+
+    expect(parsedLockeFile.object["@babel/code-frame@^7.0.0"].version).toBe(
+      parsedLockeFile.object["@babel/code-frame@^7.8.3"].version
+    );
+  });
+
+  it("parses pnpm-lock.yaml properly", async () => {
+    const packageRoot = await setupFixture("basic-pnpm");
+    const parsedLockeFile = await parseLockFile(packageRoot);
+
+    expect(Object.keys(parsedLockeFile.object["yargs@16.2.0"].dependencies!)).toContain(
+      "cliui"
     );
   });
 });
