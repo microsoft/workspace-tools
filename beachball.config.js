1aca866df2443581ce221c08b8e783a7054fcd7d
Elizabeth Craig <elcraig@microsoft.com>
2021-10-21 13:00:48 -0700

    Update beachball and add ignorePatterns (#81)
    * Update beachball and add ignorePatterns
    
    * Change files

--- beachball.config.js
+++ beachball.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   scope: ["!src/__fixtures__/**/*"],
+  ignorePatterns: [
+    ".github/**",
+    ".prettierrc",
+    "jest.config.js",
+    "src/__fixtures__/**",
+    "src/__tests__/**",
+    // This prevents dependabot from being blocked by change file requirements for lock file-only changes
+    "yarn.lock",
+  ],
 };
