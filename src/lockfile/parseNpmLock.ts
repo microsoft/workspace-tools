cdac68ac921b154de044e77f79e0164258ae534c
Ria Carmin <riacarmin@microsoft.com>
2021-11-29 16:31:18 -0800

    Rewrites types in a different style

--- src/lockfile/parseNpmLock.ts
+++ src/lockfile/parseNpmLock.ts
@@ -1,6 +1,6 @@
 import { ParsedLock, NpmLockFile } from "./types";
 
 export const parseNpmLock = (lock: NpmLockFile): ParsedLock => ({
-  object: lock.dependendcies ?? {},
+  object: lock.dependencies ?? {},
   type: "success",
 });
--- src/lockfile/types.ts
+++ src/lockfile/types.ts
@@ -16,28 +16,28 @@ export interface PnpmLockFile {
   packages: { [name: string]: any };
 }
 
-export interface NpmWorspacesInfo {
-  version: "string";
-  workspaces: Record<"packages", string[]>;
+export interface NpmWorkspacesInfo {
+  version: string;
+  workspaces: { packages: string[] };
 }
 
-export interface NpmSynlinkInfo {
-  resolved: string; // Where the package is  reslved from.
+export interface NpmSymlinkInfo {
+  resolved: string; // Where the package is  resolved from.
   link: boolean; // A flag to indicate that this is a symbolic link.
-  integrity?: 'sha512' | 'sha1';
+  integrity?: "sha512" | "sha1";
   dev?: boolean;
   optional?: boolean;
   devOptional?: boolean;
-  dependendcies?: { [key in string]: LockDependency; }
+  dependencies?: { [key: string]: LockDependency };
 }
 
 export interface NpmLockFile {
   name: string;
   version: string;
-  lockfileVersion?: number; // 1: v5, v6; 2: backwards compatible v7; 3: non-backwards compatible v7 
+  lockfileVersion?: 1 | 2 | 3; // 1: v5, v6; 2: backwards compatible v7; 3: non-backwards compatible v7
   requires?: boolean;
   packages?: {
-    ""?: NpmWorspacesInfo, // Monorepo root
-  } & Record<string, NpmSynlinkInfo | LockDependency>;
-  dependendcies?: { [key in string]: LockDependency; }
+    ""?: NpmWorkspacesInfo; // Monorepo root
+  } & { [key: string]: NpmSymlinkInfo | LockDependency };
+  dependencies?: { [key: string]: LockDependency };
 }
