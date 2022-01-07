b8b350435288fffce168ec27afd7ad4a100d5f56
Ria Carmin <riacarmin@microsoft.com>
2022-01-06 17:14:53 -0800

    Makes parseLockFile and queryLockFile compatible

--- src/lockfile/parseNpmLock.ts
+++ src/lockfile/parseNpmLock.ts
@@ -1,6 +1,28 @@
-import { ParsedLock, NpmLockFile } from "./types";
+import { nameAtVersion } from "./nameAtVersion";
+import { ParsedLock, NpmLockFile, LockDependency } from "./types";
 
-export const parseNpmLock = (lock: NpmLockFile): ParsedLock => ({
-  object: lock.dependencies ?? {},
-  type: "success",
-});
+type FormatNpmLock = (
+  previousValue: { },
+  currentValue: [string, LockDependency],
+  currentIndex: number,
+  array: [string, LockDependency][]
+) => { [x: string]: LockDependency; };
+
+/**
+ * formatNpmLock reformats the dependencies object, so the key includes the version, similarly to yarn.lock. For
+ * example, `"@microsoft/task-scheduler": { }` will become `"@microsoft/task-scheduler@2.7.1": { }`.
+ */
+const formatNpmLock: FormatNpmLock = (previousValue, currentValue) => {
+  const [key, dependency] = currentValue;
+  previousValue[nameAtVersion(key, dependency.version)] = dependency;
+  return previousValue;
+};
+
+export const parseNpmLock = (lock: NpmLockFile): ParsedLock => {
+  const dependencies = Object.entries(lock.dependencies ?? {}).reduce(formatNpmLock, {});
+
+  return {
+    object: dependencies,
+    type: "success",
+  };
+};
