7e302ad45fd205afd84e4a3e64d7e3ff2f6b2f3c
Elizabeth Craig <elcraig@microsoft.com>
2022-06-23 12:22:50 -0700

    Add findProjectRoot path helper

--- src/paths.ts
+++ src/paths.ts
@@ -1,10 +1,9 @@
-import path from 'path';
-import fs from 'fs';
+import path from "path";
+import fs from "fs";
+import { getWorkspaceRoot } from "./workspaces/getWorkspaceRoot";
 
 /**
- * Starting from `cwd`, searches up the directory hierarchy for `pathName`
- * @param pathName
- * @param cwd
+ * Starting from `cwd`, searches up the directory hierarchy for `pathName`.
  */
 export function searchUp(pathName: string, cwd: string) {
   const root = path.parse(cwd).root;
@@ -27,19 +26,41 @@ export function searchUp(pathName: string, cwd: string) {
   return null;
 }
 
+/**
+ * Starting from `cwd`, searches up the directory hierarchy for `.git`.
+ */
 export function findGitRoot(cwd: string) {
-  return searchUp('.git', cwd);
+  return searchUp(".git", cwd);
 }
 
+/**
+ * Starting from `cwd`, searches up the directory hierarchy for `package.json`.
+ */
 export function findPackageRoot(cwd: string) {
-  return searchUp('package.json', cwd);
+  return searchUp("package.json", cwd);
 }
 
+/**
+ * Starting from `cwd`, searches up the directory hierarchy for the workspace root,
+ * falling back to the git root if no workspace is detected.
+ */
+export function findProjectRoot(cwd: string) {
+  let workspaceRoot: string | undefined;
+  try {
+    workspaceRoot = getWorkspaceRoot(cwd);
+  } catch {}
+
+  return workspaceRoot || findGitRoot(cwd);
+}
+
+/**
+ * Get the folder containing beachball change files.
+ */
 export function getChangePath(cwd: string) {
   const gitRoot = findGitRoot(cwd);
 
   if (gitRoot) {
-    return path.join(gitRoot, 'change');
+    return path.join(gitRoot, "change");
   }
 
   return null;
