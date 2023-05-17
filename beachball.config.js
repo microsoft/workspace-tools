1ae1a23fe0402f213d6ef2c5fad221a9bacab185
Elizabeth Craig <elcraig@microsoft.com>
2023-05-16 18:24:08 -0700

    beachball sync

--- beachball.config.js
+++ beachball.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+// @ts-check
+/** @type {import('beachball').BeachballConfig} */
+const config = {
+  access: "public",
+  disallowedChangeTypes: ["major"],
+  groupChanges: true,
   scope: ["!**/__fixtures__/**"],
   ignorePatterns: ["**/jest.config.js", "**/src/__fixtures__/**", "**/src/__tests__/**"],
 };
+module.exports = config;
