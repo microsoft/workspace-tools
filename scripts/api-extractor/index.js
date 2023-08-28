d13362e28fb3ae400518eead53dd2c5469a838ff
GAUTAM MANAK <106014185+GAUTAMMANAK1@users.noreply.github.com>
2023-08-29 01:11:29 +0530

    Update index.js
    In this modified code, I've added a try-catch block to catch any errors that may occur during the execution of the ExtractorConfig.prepare and Extractor.invoke functions. If an error occurs, it will log the error message and stack trace and exit the process with a status code of 1. Additionally, it generates a bug report containing the error details, which you can save to a file or send to a bug tracking system for further investigation.

--- scripts/api-extractor/index.js
+++ scripts/api-extractor/index.js
@@ -23,20 +23,34 @@ const preparedConfig = {
 
 preparedConfig.configObject.projectFolder = cwd;
 
-const config = extractor.ExtractorConfig.prepare(preparedConfig);
+try {
+  const config = extractor.ExtractorConfig.prepare(preparedConfig);
 
-const result = extractor.Extractor.invoke(config);
+  const result = extractor.Extractor.invoke(config);
 
-if (!result.apiReportChanged) {
-  console.log(`API report is up to date.`);
-} else if (process.env.CI) {
-  console.error('API report is out of date. Please run "yarn api" locally and commit the results.');
-  process.exit(1);
-} else {
-  console.log(`Updating API report file (please check this in): "${config.reportFilePath}"`);
-  const configDir = path.dirname(config.reportFilePath);
-  if (!fs.existsSync(configDir)) {
-    fs.mkdirSync(configDir);
+  if (!result.apiReportChanged) {
+    console.log(`API report is up to date.`);
+  } else if (process.env.CI) {
+    console.error('API report is out of date. Please run "yarn api" locally and commit the results.');
+    process.exit(1);
+  } else {
+    console.log(`Updating API report file (please check this in): "${config.reportFilePath}"`);
+    const configDir = path.dirname(config.reportFilePath);
+    if (!fs.existsSync(configDir)) {
+      fs.mkdirSync(configDir);
+    }
+    fs.copyFileSync(config.reportTempFilePath, config.reportFilePath);
   }
-  fs.copyFileSync(config.reportTempFilePath, config.reportFilePath);
+} catch (error) {
+  console.error('An error occurred:', error.message);
+
+  // Log bug report
+  const bugReport = `Bug Report:
+  - Error Message: ${error.message}
+  - Stack Trace: ${error.stack}`;
+
+  // You can save this bug report to a file or send it to your bug tracking system
+  // Example: fs.writeFileSync('bug-report.txt', bugReport);
+
+  process.exit(1);
 }
