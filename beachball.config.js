7498111d18b8cdfe47a2ea0e1e26ee3ed6cc806a
Elizabeth Craig <elcraig@microsoft.com>
2023-10-19 09:35:44 -0700

    Remove beachball workspace-tools resolutions (#329)

--- beachball.config.js
+++ beachball.config.js
@@ -1,8 +1,5 @@
 // @ts-check
 
-const { spawnSync } = require("child_process");
-const fs = require("fs");
-
 /** @type {import('beachball').BeachballConfig} */
 const config = {
   access: "public",
@@ -10,33 +7,5 @@ const config = {
   groupChanges: true,
   scope: ["!**/__fixtures__/**"],
   ignorePatterns: ["**/jest.config.js", "**/src/__fixtures__/**", "**/src/__tests__/**"],
-  hooks: {
-    postbump: (packagePath, name) => {
-      if (name !== "workspace-tools") {
-        return;
-      }
-
-      // This has to be loaded here because the package won't be built yet during checkchange in CI
-      const { getUnstagedChanges } = require("workspace-tools");
-
-      if (getUnstagedChanges(process.cwd()).includes("yarn.lock")) {
-        console.warn("yarn.lock unexpectedly had changes; not updating workspace-tools resolutions");
-        return;
-      }
-
-      let yarnLock = fs.readFileSync("yarn.lock", "utf-8");
-      const wsToolsMatch = yarnLock.match(/.*workspace-tools@npm:workspace-tools@latest[\s\S]+?\n\n/);
-      if (wsToolsMatch) {
-        console.log("Removing workspace-tools entry from yarn.lock");
-        yarnLock = yarnLock.replace(wsToolsMatch[0], "");
-        fs.writeFileSync("yarn.lock", yarnLock);
-
-        console.log("Running yarn to update workspace-tools resolutions");
-        spawnSync("yarn", ["--ignore-scripts"], { stdio: "inherit" });
-      } else {
-        console.warn("Didn't find a yarn.lock entry for workspace-tools resolutions in expected format");
-      }
-    },
-  },
 };
 module.exports = config;
